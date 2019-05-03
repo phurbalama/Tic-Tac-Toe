8e9b2239418a28379d331349f6c35d34c1fc5328
Phurba Lama <phurbalama2002@gmail.com>
2019-05-02 23:42:25 -0400

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,27 +1,47 @@
 
-//function to validate the move of the user
-$(function() {
+$(function () {
     var xOrO = "X";
     var count = 0;
-    
-    //gets table body and table data element to change the value of empty boxes
-    $("tbody td").click(function(){
+
+    $("tbody td").click(function () {
         var currentValue = $(this).text();
-        if(currentValue != "?" )
-        {
-            $("tfoot td").text("Stop Cheating, this move is illegal");
+        if (currentValue != "?") {
+            $("tfoot td").text("Stop cheating, this move is illegal.");
             return;
         }
 
         count++;
-        $("tfoot td").text("Number of moves "+ count);
-      $(this).text(xOrO);
-      xOrO = xOrO == "X" ? "O" : "X";
+        $("tfoot td").text("Number of moves " + count);
+
+
+        $(this).text(xOrO);
+        xOrO = xOrO == "X" ? "O" : "X";
+
+        var status = {
+            whoWon: "X",
+            winningCombo: [0, 1, 2],
+            isWinner: false
+        };
+
+        var moves = $("tbody td").text();
+
+        $.get("tictactoe?moves=" + moves  , (data, status) => {
+                console.log({data, status});
+        });
+
+        if (status.isWinner == true) {
+            var tds = $("tbody td");
+            var p1, p2, p3;
+
+            p1 = status.winningCombo[0];
+            p2 = status.winningCombo[1];
+            p3 = status.winningCombo[2];
+
+            $(tds[p1]).addClass("winner");
+            $(tds[p2]).addClass("winner");
+            $(tds[p3]).addClass("winner");
+
+        }
+
     });
-    
-
-
-})
-
-
-
+});
